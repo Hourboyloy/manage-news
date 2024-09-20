7b9346f095729c764596ba4e62f90d0445573029
Hourboyloy <chhoysienghour@gmail.com>
2024-09-20 14:53:41 +0700

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,7 +10,7 @@ const background_route = require("./src/route/background.route");
 
 app.use(
   cors({
-    origin: "https://client-delta-indol.vercel.app",
+    origin: "https://manage-news-client134.vercel.app",
     methods: "GET,POST,DELETE,PUT",
   })
 );
