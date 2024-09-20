dcc839cfb6aab43c9a529ce149f1b69b238c016a
Hourboyloy <chhoysienghour@gmail.com>
2024-09-20 14:01:08 +0700

    first commit

---
+++ nodejs/src/database/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const uri = process.env.URL_DATABSE || "your_mongodb_uri";
+const mymongodb = async () => {
+    try {
+      await mongoose.connect(uri, {
+        // useNewUrlParser: true
+      });
+      console.log("Successfully connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    }
+};
+module.exports = { mymongodb };
