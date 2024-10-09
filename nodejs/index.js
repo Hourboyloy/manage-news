349d68d29acb6e1009f562088be4233b920cb32a
Hourboyloy <chhoysienghour@gmail.com>
2024-10-09 21:51:20 +0700

    last commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -35,7 +35,7 @@ news_route(app);
 background_route(app);
 
 // cron schedule is runing at 5am and 5 pm
-cron.schedule("45 9,17 * * *", () => {
+cron.schedule("0 5,22 * * *", () => {
   startScrapeData1();
   console.log("CRON job scheduled. Application is running.");
 });
