07229fe0f952cb0da470cd462653d1a7bc332541
Hourboyloy <chhoysienghour@gmail.com>
2024-10-14 20:33:42 +0700

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -62,7 +62,7 @@ Categories(app);
 ScrapingDataFromOtherWebsite(app);
 
 // Schedule the cron job to scrape data every minute
-const job = schedule.scheduleJob("*/15 20 * * *", () => {
+const job = schedule.scheduleJob("* 20 * * *", () => {
   Promise.all([
     startScrapeDataFromRFI(),
     startScrapeDataFromKhmerNoteTechnology(),
